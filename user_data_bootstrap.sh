dd72e9f0ae3a0245f318f1da917e8eaa522ac843
Hanif <77610396+ferdianhanif@users.noreply.github.com>
2026-08-09 15:06:19 +0700

    Delete user_data_bootstrap.sh

--- user_data_bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Amazon EC2 Automated User Data Bootstrap Script
-# Purpose: Automatically installs, enables, and starts Apache Web Server on boot.
-
-yum -y install httpd
-systemctl enable httpd
-systemctl start httpd
-echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
